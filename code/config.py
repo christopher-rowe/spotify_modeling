9c04922e35b1bf5bbbf99082dc5e0cc0a8879d94
Chris Rowe <chris.lloyd.rowe@gmail.com>
2020-08-23 13:22:52 -0700

    updating config.py

--- code/config.py
+++ code/config.py
@@ -1,20 +1,35 @@
-
 # File name: config.py
 # Description: To initialize spotify authenticaion parameters
-#              and other objects
 # Author: Chris Rowe
 
 # initialize authentication parameters
-username = 'xxx' # Spotify username
-client_id ='xxx' # Spotify cliend id from registered app
-client_secret = 'xxx' # Spotify cliend secret from registered app
-redirect_uri = 'xxx' # arbitrary redirect_uri from registered app 
-scope = 'xxx' # scope (see API documentation for details)
+username = 'crowe@ucla.edu'
+client_id ='fcac9ea643224c98a34ab59b6b826178'
+client_secret = '4026fe3681a64c228597fd34ac7b4671'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-recently-played playlist-read-private playlist-modify-private'
 
 # initialize good playlist names
 # description: this is a list of personal playlists that are used
 #              to engineer the stage 1 playlist outcome
-good_playlist_names = ['playlist1', 'playlist2', 'playlist3']
+good_playlist_names = ['KM', 'if then was now', '2020',
+                       'Bad Ass Folk Women', 'Robotix',
+                       'BM', 'psychedelic', 'cloud',
+                       'BRC 2019', '1964', '1963-1969',
+                       'HEADY SHIT', '2019', '2018',
+                       '2017', '2016', 'August 2015',
+                       'B', 'A']
 
-# target playlist id for data candidates (obtainable via spotify API)
-target_playlist = 'xxx'
+# this is the playlist id for the data_candidates playlist
+target_playlist = '69SFgw78N4seDVXqZNTPa8'
+
+# archive code to get list of user's playlists (this is helpful for identifying the 
+#   name of the data_candidates playlist)
+#url = 'https://api.spotify.com/v1/users/' + userid + '/playlists'
+#headers = {
+#'Accept': 'application/json',
+#'Content-Type': 'application/json',
+#'Authorization': f'Bearer ' + token,
+#}
+#response = requests.get(url, headers = headers, timeout = 5)  
+#response.json()['items'][0]
